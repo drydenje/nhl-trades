14e494da550bc0d80afea3e09d7196946dedc1bc
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-09-10 22:47:31 -0400

    more axios mocking, not quite working

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,12 +1,25 @@
-import axios from "axios";
+// import axios from "axios";
+const axios = require("axios");
 jest.mock("axios");
 import { describe, expect, test } from "@jest/globals";
 import { getTradeTeams } from "./utils";
 import page from "../../public/result";
 
+const BASE_URL = `https://www.nhltradetracker.com/user/trade_list_by_season`;
+
+type Trade = {};
+
 describe("getTradeTeams module", () => {
-  test("should get the trades", () => {
-    axios.get.mockResolvedValue(page);
+  test("should get the trades from a cheerio object", () => {
+    const users = [{ name: "Bob" }];
+    const resp = { data: { name: "Aud" } };
+    // axios.get.mockResolvedValue(resp);
+
+    return getTradeTeams(`${BASE_URL}/1919-20/1`).then((data) => {
+      expect(data).toEqual(users);
+      // console.log(data);
+    });
+
     // expect(getTradeTeams(5)).toBe(5);
   });
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,22 @@
-const getTradeTeams = (num) => {
-  return num;
+const cheerio = require("cheerio");
+
+const getTradeTeams = async (url: string) => {
+  const axios = require("axios");
+  const axiosResponse = await axios.request({
+    method: "GET",
+    url: url,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    },
+  });
+
+  return axiosResponse.data;
+
+  // const $ = cheerio.load(axiosResponse.data);
+  // const container = $("#container");
+
+  // return container;
 };
 
 export { getTradeTeams };
