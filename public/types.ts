32ca2d80170e1861f2075fe2ad0129e479afad18
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-10-01 22:31:15 -0400

    playing with types

---
+++ public/types.ts
@@ -0,0 +1,40 @@
+type Player = {
+  hockeyDBID: number,
+  firstName: string,
+  lastName: string,
+  dob: string,
+  city: string,
+  state: string,
+  country: string,
+  stats: {
+    season: string,
+    team: string,
+    league: string,
+    regular: {
+      gp: number,
+      g: number,
+      a: number,
+      pts: number,
+      pim: number,
+      pm: number,
+    }
+    playoffs: {
+      gp: number,
+      g: number,
+      a: number,
+      pts: number,
+      pim: number,
+      pm: number,
+    }
+  }[]
+}
+
+
+
+type Trade = {
+  date: string,
+  teams: {
+    ID: 
+    name: 
+  }[]
+}
