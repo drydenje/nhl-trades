437c65e2f51f962c4539da44115f3b4093a972b5
Jeremy <jeremy.dryden@gmail.com>
2024-05-29 23:14:00 -0400

    trying to combine two objects elegantly (all players in array). This will be used to bring together id numbers from multiple sources of information

--- src/index.js
+++ src/index.js
@@ -9,27 +9,95 @@ import fs from "fs";
 // convertToCSV(DRAFT_RESULTS_JSON);
 // writeCSVFile(`./public/scraped-data/draft-results-test.json`);
 
-import { players } from "./player-nhl-id";
-const headings = ["id", "name"];
+// import { players } from "./player-nhl-id";
+import { hrPlayers } from "./hr-player-id-copy";
+const headings = ["id", "name", "hrId"];
 
-const playerLines = players.map((line) => {
-  return {
-    id: line.id,
-    name: line.name,
-  };
-});
+const players = [
+  {
+    id: 8444924,
+    name: "Jim Anderson",
+  },
+  {
+    id: 8444926,
+    name: "Murray Anderson",
+  },
+  {
+    id: 8444995,
+    name: "Pete Backor",
+  },
+  {
+    id: 8444893,
+    name: "Jim Agnew",
+  },
+];
 
-const csv = convertArrayToCSV(playerLines, {
-  headings,
-  separator: ",",
-});
+const hr = [
+  { name: "Jim Anderson", hrID: "anderji01" },
+  { name: "Murray Anderson", hrID: "andermu01" },
+  { name: "Pete Backor", hrID: "backope01" },
+];
 
-fs.writeFile("./public/final-csv-files/player-nhl-ids.csv", csv, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  // console.log(`Data written to: ${csvFilePath}`);
-});
+// const h = new Set(hr);
+// const i = new Set(players);
+// const u = h.union(i);
+// console.log(h.union(i));
+const array1 = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+];
+const array2 = [
+  { hrID: "anderji01", name: "Jane" },
+  { id: 3, name: "Doe" },
+];
+
+// Merging arrays with unique values using Map and Set
+const map = new Map([...array1, ...array2].map((obj) => [obj.name, obj]));
+const mergedArray = Array.from(map.values());
+console.log(mergedArray);
+
+// two 'Ron Anderson's
+// const test = hrPlayers["a"].filter((player) => player.name === "Ron Anderson");
+
+// players.forEach((player) => {
+//   const test = hrPlayers["a"].filter(
+//     (hrPlayer) => hrPlayer.name === player.name
+//   );
+
+//   const obj = {
+//     id: player.id,
+//     name: player.name,
+//     ...(test[0] ? { hrID: test[0].hrID } : null),
+//   };
+
+//   // might have multiple similar names
+//   // console.log(test);
+//   console.log(obj);
+// });
+
+// if (test.length === 1) {
+//   console.log(test[0].hrID);
+// }
+
+// const playerLines = players.map((line) => {
+
+//   return {
+//     id: line.id,
+//     name: line.name,
+//   };
+// });
+
+// const csv = convertArrayToCSV(playerLines, {
+//   headings,
+//   separator: ",",
+// });
+
+// fs.writeFile("./public/final-csv-files/player-nhl-ids.csv", csv, (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   // console.log(`Data written to: ${csvFilePath}`);
+// });
 
 // import { teams } from "./team-nhl-id";
 // const headings = [
