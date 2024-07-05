aabe4c8345fa6f168f16fd17551ef7413b8556df
Jeremy <jeremy.dryden@gmail.com>
2024-07-04 23:59:38 -0400

    close

--- src/experiment.js
+++ src/experiment.js
@@ -1,36 +1,90 @@
 const chalk = require("chalk");
-const getPage = require("./utils/utils.js");
 
 // VGK 20232024
 
-const runScrape = async () => {
-  let delay = 0;
-  const delayIncrement = 1000;
-  const startTime = new Date();
+// const runScrape = async () => {
+//   let delay = 0;
+//   const delayIncrement = 1000;
+//   const startTime = new Date();
 
-  // let results = Promise.all(tasks).then((results) => {
-  //   console.log("results: " + results);
-  // });
+//   // let results = Promise.all(tasks).then((results) => {
+//   //   console.log("results: " + results);
+//   // });
 
-  const endTime = new Date();
-  const timeElapsed = (endTime - startTime) / 1000;
-  console.log("timeElapsed:", timeElapsed);
-};
+//   const endTime = new Date();
+//   const timeElapsed = (endTime - startTime) / 1000;
+//   console.log("timeElapsed:", timeElapsed);
+// };
+const notUsedDeleteLater = true;
 
-const getTeamRosters = (teamToScrape, startYear) =>
-  async function* () {
-    let year = startYear;
-    let url = `https://api-web.nhle.com/v1/roster/${teamToScrape}/${year}`;
-    console.log(teamToScrape);
-    console.log(url);
-    while (year) {
+// Star Wars characters example
+// for await (const page of starWarsCharacters()) {
+//   for (const char of page) {
+//     console.log(char.name);
+//   }
+// }
+
+(async function () {
+  for await (const roster of nhlTeamRosters("SEA", "20232025")) {
+    console.log(roster[0]);
+  }
+})();
+
+async function* nhlTeamRosters(teamToScrape, startYear) {
+  let year = startYear;
+  let url = `https://api-web.nhle.com/v1/roster/${teamToScrape}/${year}`;
+  while (year) {
+    console.log(
+      chalk.yellow.bgBlue(`Trying to scrape: [${teamToScrape}] -> ${year}`)
+    );
+
+    try {
       const response = await fetch(url);
-      const data = await response.json().then((res) => console.log(res));
+      if (!response.ok) {
+        year = null;
+        console.log("NOPE");
+        // throw new Error(`Response status: ${response.status}`);
+      } else {
+        year = year - 10001;
+      }
 
-      // if there is a '404', set to null
-      url = null;
-      yield* data.results;
+      const data = await response
+        .json()
+        .then((res) => [...res.forwards, ...res.defensemen, ...res.goalies])
+        .then((res) =>
+          res.map((player) => {
+            return {
+              ...player,
+              hrID: null,
+              hdbID: null,
+              verified: false,
+            };
+          })
+        );
+
+      await delay(3);
+      yield data;
+    } catch (error) {
+      console.error(error.message);
     }
-  };
 
-export { runScrape, getTeamRosters };
+    // if there is a '404', set to null
+  }
+}
+
+const delay = (seconds) =>
+  new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+
+async function* starWarsCharacters() {
+  let nextUrl = `https://swapi.dev/api/people`;
+  while (nextUrl) {
+    const response = await fetch(nextUrl);
+    const data = await response.json(); //.then((res) => console.log(res));
+
+    // console.log("resp:", response.json());
+    nextUrl = data.next;
+    console.log("NEXT:", nextUrl);
+    await delay(5);
+    yield data.results;
+  }
+}
