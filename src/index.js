a4e7e715addf1364ab08d605cb6294ea89580d07
Jeremy <jeremy.dryden@gmail.com>
2024-07-21 22:21:17 -0400

    quick cleanup

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ import { scrapeHDBPlayers } from "./player-data/hockey-db-id-scraper.js";
 
 var neo4j = require("neo4j-driver");
 (async () => {
-  // URI examples: 'neo4j://localhost', 'neo4j+s://xxx.databases.neo4j.io'
-  // const URI = "";
-  // const USER = "";
-  // const PASSWORD = "";
   let driver;
 
   try {
